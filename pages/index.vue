2a09e5f73b662e718daf04594112e24418e6048e
“faye-at-603815615736” <67593261+FSipiano@users.noreply.github.com>
2020-12-10 11:00:21 +0000

    sm: px-6

--- pages/index.vue
+++ pages/index.vue
@@ -33,14 +33,14 @@
             GitHub
           </a>
         </div>
-        <p class="paragraph pt-24 px-36 ">
+        <p class="paragraph pt-24 px-36 sm: px-6">
           Hi, I'm Faye. I love coding, writing, and driving. I also ride horses
           and cook.
         </p>
-        <p class="paragraph pt-6">I write copy for multiple
+        <p class="paragraph pt-6 sm: px-6">I write copy for multiple
           industries, including Education, Transport and have won award entries in Finance.
         </p>
-        <p class="paragraph py-6">
+        <p class="paragraph py-6 sm: px-6">
           I design and build beautiful websites using Vue.js, Nuxt.js, and
           Tailwind.
         </p>
@@ -48,7 +48,7 @@
         <div class="justify-center mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
           <nuxt-link to="/contact"
           type="button"
-              class="ml-3 inline-flex items-center px-12 py-6 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              class="ml-3 inline-flex items-center px-12 py-6 border border-transparent rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 
             >
               Contact
