cde85a4a05b61a516fef732384b8f07f1880baac
“faye-at-603815615736” <67593261+FSipiano@users.noreply.github.com>
2021-02-03 10:05:03 +0000

    increased button font weight

--- components/ContactButton.vue
+++ components/ContactButton.vue
@@ -1,6 +1,6 @@
 <template>
   <nuxt-link to="/contact"
-              class="ml-3 inline-flex items-center px-12 py-6 border border-transparent rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              class="ml-3 text-md inline-flex font-bold items-center px-12 py-6 border border-transparent rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 
             >
               Contact
