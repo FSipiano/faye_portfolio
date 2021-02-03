a2c300dd109a9a410a8e07413f7fc01fb48f1f50
“faye-at-603815615736” <67593261+FSipiano@users.noreply.github.com>
2021-02-03 09:25:59 +0000

    Improved text contrast

--- pages/contact.vue
+++ pages/contact.vue
@@ -12,7 +12,7 @@
         <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">
           Let's work together to create your vision
         </h2>
-        <p class="text-lg text-gray-600 sm:mt-3">
+        <p class="text-lg text-gray-700 sm:mt-3">
           I’d love to hear from you! Send me an email,
 
         <a class="text-lg mb-6 text-indigo-600 text-decoration: underline sm:mt-3"
@@ -23,7 +23,7 @@
           I'll respond as soon as I can!
         </p>
 
-        <p class="text-lg text-gray-600 sm:mt-3">Email to:</p>
+        <p class="text-lg text-gray-700 sm:mt-3">Email me at:</p>
         <a class="text-2xl mb-6 text-indigo-600 text-decoration: underline sm:mt-3"
         href="mailto:fayesipiano@outlook.com">
           fayesipiano@outlook.com
@@ -31,24 +31,24 @@
 
         <form enctype=”multipart/form-data” action="https://formsubmit.co/fayesipiano@outlook.com" method="POST" class="mt-12 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
           <div>
-            <label for="first_name" class="block text-sm font-medium text-gray-600">First name</label>
+            <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
             <div class="mt-1">
               <input
               type="text"
               name="first_name" id="first_name"
-              autocomplete="given-name" required
-              class="input border border-gray-600 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600 autofocus">
+              autocomplete="given-name" required autofocus
+              class="input border border-gray-700 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600">
             </div>
           </div>
           <div>
-            <label for="last_name" class="block text-sm font-medium text-gray-600 required">Last name</label>
+            <label for="last_name" class="block text-sm font-medium text-gray-700 required">Last name</label>
             <div class="mt-1">
               <input type="text" name="last_name" id="last_name" autocomplete="family-name" required
               class="input border border-gray-600 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600 required">
             </div>
           </div>
           <div class="sm:col-span-2">
-            <label for="email" class="block text-sm font-medium text-gray-600">Email</label>
+            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
             <div class="mt-1">
               <input id="email" name="email" type="email" required
               autocomplete="email"
@@ -56,7 +56,7 @@
             </div>
           </div>
           <div class="sm:col-span-2">
-            <label for="company" class="block text-sm font-medium text-gray-600">Company</label>
+            <label for="company" class="block text-sm font-medium text-gray-700">Company</label>
             <div class="mt-1">
               <input
               type="text"
@@ -70,9 +70,9 @@
             <div class="flex justify-between">
               <label
               for="phone"
-              class="block text-sm font-medium text-gray-600">Phone</label>
+              class="block text-sm font-medium text-gray-700">Phone</label>
               <span id="phone_description"
-              class="text-sm text-gray-600">Optional</span>
+              class="text-sm text-gray-700">Optional</span>
             </div>
             <div class="mt-1">
               <input
@@ -88,10 +88,10 @@
               <label
               for="how_can_we_help"
               required
-              class="block text-sm  pr-6 font-medium text-gray-600">What did you have in mind?
+              class="block text-sm  pr-6 font-medium text-gray-700">What did you have in mind?
                 I can help you to design, develop and build, write, market and strategise
               </label>
-              <span id="how_can_we_help_description" class="text-sm text-gray-600">Max. 500 characters</span>
+              <span id="how_can_we_help_description" class="text-sm text-gray-700">Max. 500 characters</span>
             </div>
             <div class="mt-1">
               <textarea id="how_can_we_help"
