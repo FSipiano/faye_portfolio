a861c80134afd8813a25879de2410033111c7aff
F Sipiano <67593261+FSipiano@users.noreply.github.com>
2021-05-15 22:00:23 +0100

    Sorted github button and reduced contact button padding

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@
             href="https://github.com/FSipiano"
             target="_blank"
             rel="noopener noreferrer"
-            class="ml-3 font-bold inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            class="ml-3 font-bold inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-md font-medium text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             GitHub
           </a>
