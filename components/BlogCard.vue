59842f2903ea9e208ffab9ffe054ecd9956ab94c
F Sipiano <67593261+FSipiano@users.noreply.github.com>
2021-05-27 09:29:19 +0100

    Update BlogCard.vue

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="relative bg-gray-50 pt-10 pb-10 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+<div class="relative bg-gray-50 pt-3 pb-2 px-4 sm:px-6 lg:pt-14 lg:pb-18 lg:px-8">
   <div class="absolute inset-0">
     <div class="bg-white h-1/3 sm:h-2/3">
     </div>
@@ -7,15 +7,15 @@
   <div class="relative hover:opacity-50 max-w-7xl mx-auto">
    <a :href=post.url>
     
-    <div class="mt-4 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+    <div class="mt-2 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
       <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
         <div class="flex-shrink-0">
           <img class="h-48 w-full object-cover" :src=post.cover_image>
         </div>
         <div class="flex-1 bg-white p-6 flex flex-col justify-between">
           <div class="flex-1">
-            <p class="text-sm font-medium text-grey-500">
-              <a class="font-semibold">
+            <p class="text-sm font-medium text-grey-300">
+              <a class="">
                 {{post.description}}
               </a>
             </p>
